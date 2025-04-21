481f52fa9948f0da2de748fa1c1277b7a64bcdcd
nanini0 <alvaro.robles15081999@gmail.com>
2025-04-21 17:28:31 -0400

    clase de js

---
+++ app.js
@@ -0,0 +1,62 @@
+document.write("<h1> hola mundo <h1>");
+
+/* comentario : trabajar con la console */
+//comentario de una sola linea 
+
+
+console.log("datos de js");
+
+//tipos de datos
+
+let saludo = "hola mundo";
+
+console.log(saludo);
+
+//array 
+let nombres=['ana','jose','carlos'];
+console.log(nombres);
+//objeto 
+let objeto = {nombre:'juan',edad:13,notas:[2.5,4,6]}
+
+console.log(objeto);
+
+
+let num1 = 1;
+let num2 = 30;
+
+let resultado = num1 + num2;
+
+console.log("la suma es ",resultado);
+
+let nombre="Carlos";
+let apellido="Leon";
+
+let nombre_completo = nombre +" "+ apellido;
+console.log(nombre_completo)
+
+document.write("</br>"+"nombre"+" " + nombre_completo)
+
+/* 
+!= distinto
+== comparacion
+===
+
+*/
+
+let one =1;
+let one_again="uno";
+
+let comparar = one === one_again;
+console.log(comparar)
+
+// if 
+
+let uno = 1;
+let dos = '1';
+
+if (uno ===dos){
+    console.log("gg")
+}else{
+    console.log("asdasdas")
+}
+
